8ea4f4a7d9e453f99a87d369024cfddff619bf84
Royyyy <410310446@qq.com>
2018-01-10 22:24:29 +0800

    test 入口文件2

--- application/config.php
+++ application/config.php
@@ -293,7 +293,8 @@ return [
 	//视图输出字符串内容替换
 	'view_replace_str'  =>[
 		'__PUBLIC__' => 'public/',
-		'__DIR__'    => '/'
+		'__DIR__'    => '/',
+		'__STATIC__'    => 'static/'
 	],
 
 	'session'        =>[
@@ -301,4 +302,5 @@ return [
 		'type'       =>'',
 		'auto_start' =>'true'
 	],
+
 ];
