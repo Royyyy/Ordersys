5c77bb2ac832784fe99dccaeaf643c0bbf77e952
Royyyy <410310446@qq.com>
2018-01-12 23:22:28 +0800

    完成了user的功能·和order的一小部分，明天继续加油···

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -20,4 +20,78 @@ class Order extends Controller
 	public function index(){
 		return view('user');
 	}
+
+	/**
+	 * 设定桌号功能
+	 * @param $tableId		桌号
+	 */
+	public function setTableNum($tableId,$waiterId){
+		$order = model('Order');
+		$orderBeginDate = strtotime(date("Y-m-d H:i:s"));
+		$data=[$orderBeginDate,$waiterId,0,$tableId];
+		$result = $order->insert($data);
+		$orderId = $order->getLastInsID();
+		$this->assign('orderId',$orderId);
+		return view();
+	}
+
+	/**
+	 * 点餐功能
+	 * @param $orderData	菜单内容
+	 */
+	public function cartAdd($orderData){
+		$order = model('Order');
+		$orderId = $orderData['orderId'];
+		$result = Db::table('orderdishes')->insert($orderData);
+		if ($result) {
+			return 1;
+		}else{
+			return 2;
+		}
+	}
+
+	/**
+	 * 专门设置给外带的客人
+	 * @param $orderData
+	 */
+	public function orderWaiMai($waiterId){
+		$order = model('Order');
+		$orderBeginDate = strtotime(date("Y-m-d H:i:s"));
+		$data=[$orderBeginDate,$waiterId,0];
+		$result = $order->insert($data);
+		$orderId = $order->getLastInsID();
+		$this->assign('orderId',$orderId);
+		return view();
+	}
+	/**
+	 * 买单功能
+	 * @param $orderId		订单id
+	 */
+	public function pay($orderId){
+
+	}
+
+	/**
+	 * 订单结单功能(0-正在用餐，1-准备结账，2-已经结账，3-免单订单)
+	 * @param $orderId		订单id
+	 * @param $orderState	订单状态
+	 */
+	public function orderEnd($orderId,$orderState){
+
+	}
+
+	/**
+	 * 准备结账功能
+	 * @param $orderId		订单id
+	 */
+	public function orderReady($orderId){
+
+	}
+
+	/**
+	 * 通过订单的状态来判断是否为空桌子
+	 */
+	public function showTableState(){
+
+	}
 }
